7ae06bcf3eccfdca59ab33581c987dbf6e72177f
Rizwan Ali <rizcube@gmail.com>
2015-06-03 09:02:20 +0100

    timeslots are printed on the page

---
+++ channel.js
@@ -0,0 +1,13 @@
+$(document).ready(function()
+{
+     var timeslots=["09 AM","10 AM","11 AM","12 PM","1 PM", "2 PM", "3 PM"];
+     var time = $(".time");
+     for (var i=0; i<timeslots.length; i++)
+         {
+               time.append(
+                    $("<option>").val(timeslots[i])
+                                 .text(timeslots[i])
+               );
+         }
+
+});
