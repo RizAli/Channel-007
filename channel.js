24eb8072f8514eb86b93dc7a98886d0c647d63a2
Rizwan Ali <rizcube@gmail.com>
2015-06-03 09:42:56 +0100

    it prints dynamic table to the page

--- channel.js
+++ channel.js
@@ -6,3 +6,27 @@ $(document).ready(function() {
     $("<option>").val(timeslots[i]).text(timeslots[i]));
   }
 });
+
+function addTable() {
+  var myTableDiv = document.getElementById("myDynamicTable");
+  var table = document.createElement('TABLE');
+  table.border ='1';
+
+  var tableBody = document.createElement('TBODY');
+  table.appendChild(tableBody);
+
+  for (var i=0; i<3; i++) {
+    var tr = document.createElement('TR');
+    tableBody.appendChild(tr);
+
+    for (var j=0; j<7; j++) {
+      var td = document.createElement('TD');
+      td.width = '75';
+      td.appendChild(document.createTextNode("Cell " + i + "," + j))
+      tr.appendChild(td);
+    }
+  }
+  myTableDiv.appendChild(table);
+}
+
+addTable();
